b13f75c8c961df32b94611996ee63ad6414465d2
Rittick Agarwal <rittick.agarwal@nagarro.com>
2022-06-10 11:16:27 +0530

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-const users = [];
+let users = [];
 const port = 3001;
 
 app.get("/", (req, res) => {
